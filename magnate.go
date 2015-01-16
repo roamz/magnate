5cf0aab0be0158f64f0f8897f9b0bf5f2a0b208b
Peter DeGroff <peter@roamz.com>
2015-01-16 16:29:17 +1100

    bars and operr

--- magnate.go
+++ magnate.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/rakyll/pb"
 	"labix.org/v2/mgo"
 )
 
@@ -77,27 +78,68 @@ func (r Remove) Execute(c Client) error {
 	return c.C(r.Namer).Remove(r.Selector)
 }
 
-type Runner struct {
-	Client
-	Out     io.Writer
-	Verbose bool
-	NoDry   bool
+type OpErr struct {
+	error
+	Operation
 }
 
-func (r Runner) Run(ops ...Operation) error {
-	if r.Verbose {
-		for _, op := range ops {
-			fmt.Fprintln(r.Out, op.Describe())
+func Describe(out io.Writer, ops ...Operation) error {
+	var err error
+	for _, op := range ops {
+		if _, err = fmt.Fprintln(out, op.Describe()); err != nil {
+			return err
 		}
 	}
 
-	if r.NoDry {
-		for _, op := range ops {
-			if err := op.Execute(r.Client); err != nil {
+	return err
+}
+
+func Execute(c Client, ops ...Operation) error {
+	var err error
+	for _, op := range ops {
+		if err = op.Execute(c); err != nil {
+			return OpErr{err, op}
+		}
+	}
+
+	return err
+}
+
+type Runner struct {
+	Client
+	Out         io.Writer
+	Verbose     bool
+	NoDry       bool
+	ProgressBar bool
+}
+
+func (r Runner) Run(ops ...Operation) error {
+	var bar *pb.ProgressBar
+	if r.ProgressBar {
+		bar = pb.StartNew(len(ops))
+		bar.ShowSpeed = true
+		defer bar.Finish()
+	}
+
+	var err error
+
+	for _, op := range ops {
+		if r.ProgressBar {
+			bar.Increment()
+		}
+
+		if r.Verbose {
+			if _, err = fmt.Fprintln(r.Out, op.Describe()); err != nil {
+				return err
+			}
+		}
+
+		if r.NoDry {
+			if err = op.Execute(r.Client); err != nil {
 				return err
 			}
 		}
 	}
 
-	return nil
+	return err
 }
